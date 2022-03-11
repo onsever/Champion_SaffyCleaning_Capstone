7433548cd560f081c997cb1c0885daf3f0b321d3
mark chan <mark_chan74@hotmail.com>
2022-03-11 15:09:05 -0500

    remove pods/

--- Pods/Target Support Files/Pods-Saffy Cleaning/Pods-Saffy Cleaning-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Saffy_Cleaning : NSObject
-@end
-@implementation PodsDummy_Pods_Saffy_Cleaning
-@end
--- Pods/Target Support Files/Pods-Saffy CleaningTests/Pods-Saffy CleaningTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Saffy_CleaningTests : NSObject
-@end
-@implementation PodsDummy_Pods_Saffy_CleaningTests
-@end
