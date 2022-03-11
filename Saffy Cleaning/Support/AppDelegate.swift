4b58472eb4ec2c6d330fb12e0d1e411144e21456
mark chan <mark_chan74@hotmail.com>
2022-03-10 23:18:07 -0500

    link firebase project

--- Saffy Cleaning/Support/AppDelegate.swift
+++ Saffy Cleaning/Support/AppDelegate.swift
@@ -7,15 +7,16 @@
 
 import UIKit
 import CoreData
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         UINavigationBar.appearance().tintColor = .brandDark
+        FirebaseApp.configure()
         return true
     }
 
