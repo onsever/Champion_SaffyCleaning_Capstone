bc9c3f93b5462ccf2322e4b3e6e59fb017704d0f
Onurcan Sever <onsever@icloud.com>
2022-03-12 18:40:29 -0500

    configured view

--- Saffy Cleaning/Views/UIViews/SCVerticalOrderInfoView.swift
+++ Saffy Cleaning/Views/UIViews/SCVerticalOrderInfoView.swift
@@ -11,6 +11,7 @@ class SCVerticalOrderInfoView: UIView {
 
     public let infoLabel = SCMainLabel(fontSize: 16, textColor: .brandDark)
     public let infoValue = SCMainLabel(fontSize: 16, textColor: .brandDark)
+    private var isCentered: Bool = false
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -31,6 +32,18 @@ class SCVerticalOrderInfoView: UIView {
         configureInfoValue()
     }
     
+    public init(backgroundColor: UIColor, height: CGFloat, isCentered: Bool) {
+        super.init(frame: .zero)
+        
+        self.backgroundColor = backgroundColor
+        self.height = height
+        self.isCentered = isCentered
+        
+        configure()
+        configureInfoLabel()
+        configureInfoValue()
+    }
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -43,12 +56,22 @@ class SCVerticalOrderInfoView: UIView {
         self.addSubview(infoLabel)
         infoLabel.font = .urbanistBold(size: 16)
         
-        NSLayoutConstraint.activate([
-            infoLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 5),
-            infoLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10),
-            infoLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10),
-            infoLabel.heightAnchor.constraint(equalToConstant: height),
-        ])
+        if isCentered {
+            NSLayoutConstraint.activate([
+                infoLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 5),
+                infoLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+                infoLabel.heightAnchor.constraint(equalToConstant: height),
+            ])
+            
+        }
+        else {
+            NSLayoutConstraint.activate([
+                infoLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 5),
+                infoLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10),
+                infoLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10),
+                infoLabel.heightAnchor.constraint(equalToConstant: height),
+            ])
+        }
         
     }
     
@@ -57,12 +80,21 @@ class SCVerticalOrderInfoView: UIView {
         infoValue.font = .urbanistRegular(size: 16)
         infoValue.numberOfLines = 2
         
-        NSLayoutConstraint.activate([
-            infoValue.topAnchor.constraint(equalTo: infoLabel.bottomAnchor, constant: 5),
-            infoValue.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10),
-            infoValue.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10),
-            infoValue.heightAnchor.constraint(equalToConstant: height),
-        ])
+        if isCentered {
+            NSLayoutConstraint.activate([
+                infoValue.topAnchor.constraint(equalTo: infoLabel.bottomAnchor, constant: 5),
+                infoValue.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+                infoValue.heightAnchor.constraint(equalToConstant: height),
+            ])
+        }
+        else {
+            NSLayoutConstraint.activate([
+                infoValue.topAnchor.constraint(equalTo: infoLabel.bottomAnchor, constant: 5),
+                infoValue.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10),
+                infoValue.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10),
+                infoValue.heightAnchor.constraint(equalToConstant: height),
+            ])
+        }
         
     }
 
