aff4d1629b3472422dc2803920838d21bb401f5d
Onurcan Sever <onsever@icloud.com>
2022-03-18 21:50:52 -0400

    fixed crashing issue

--- Saffy Cleaning/Controllers/Other View Controllers/AddressViewController.swift
+++ Saffy Cleaning/Controllers/Other View Controllers/AddressViewController.swift
@@ -126,31 +126,26 @@ class AddressViewController: UIViewController {
         let contactNumber = contactNumberView.getTextField().text!
         let houseType = houseTypeView.getTextField().text!
         let houseSize = houseSizeView.getTextField().text!
-        LocationSearchService.service.searchLocation(text: postalCode, completion: { [weak self](location) in
+        
+        LocationSearchService.service.searchLocation(text: postalCode, completion: { [weak self] (location) in
+            
+            guard let self = self else { return }
+            
             if let location = location {
-                DispatchQueue.main.async {
-                    let newAddress = Address(name: "", room: room, flat: flat, street: street, postalCode: postalCode, building: building, district: district, contactPerson: contactPerson, contactNumber: contactNumber, type: houseType, sizes: String(format: "%d", Int(houseSize)!), longitude: location.longitude, latitude: location.latitude)
-                    let NSDict = try! DictionaryEncoder.encode(newAddress)
-                    FirebaseDBService.service.saveAddress(value: NSDict as NSDictionary)
-                    if
-                        ((self?.isEditingMode) != nil) {
-                        self?.dataSource?.didTapSave(newAddress)
-                    } else {
-                        self?.delegate?.didTapAddButton(newAddress)
-                    }
+                
+                let newAddress = Address(name: "", room: room, flat: flat, street: street, postalCode: postalCode, building: building, district: district, contactPerson: contactPerson, contactNumber: contactNumber, type: houseType, sizes: String(format: "%d", Int(houseSize)!), longitude: location.longitude, latitude: location.latitude)
+                let NSDict = try! DictionaryEncoder.encode(newAddress)
+                FirebaseDBService.service.saveAddress(value: NSDict as NSDictionary)
+                
+                if self.isEditingMode == true {
+                    self.dataSource?.didTapSave(newAddress)
+                }
+                else {
+                    self.delegate?.didTapAddButton(newAddress)
                 }
             }
             
         })
-//        let newAddress = Address(name: "", room: room, flat: flat, street: street, postalCode: postalCode, building: building, district: district, contactPerson: contactPerson, contactNumber: contactNumber, type: houseType, sizes: String(format: "%d", Int(houseSize)!), longitude: 30, latitude: 30)
-//        let NSDict = try! DictionaryEncoder.encode(newAddress)
-//        FirebaseDBService.service.saveAddress(value: NSDict as NSDictionary)
-        
-//        if isEditingMode {
-//            dataSource?.didTapSave(newAddress)
-//        } else {
-//            delegate?.didTapAddButton(newAddress)
-//        }
         
         self.dismiss(animated: true, completion: nil)
     }
